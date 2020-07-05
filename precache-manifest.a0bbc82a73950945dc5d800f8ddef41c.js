07af24b45c4b9eb8ef49decefe4b476ca670d9a7
xrewsxkj <xrewsxkj@real.cc>
2020-07-05 15:08:40 +0900

    Updates

---
+++ precache-manifest.a0bbc82a73950945dc5d800f8ddef41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a76ea3aba91822fbf6df9d9578ebb3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c9c25494eeb0388557a9",
+    "url": "/movie_app_2020/static/css/main.6a6d7360.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c25494eeb0388557a9",
+    "url": "/movie_app_2020/static/js/main.4163455c.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
